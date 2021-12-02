2de12e6414f45d4544b97ed0727d2566da507f8e
broccolism <mile3880@gmail.com>
2021-12-02 23:00:49 +0900

    #9 :sparkles: search result screen done

--- tiny_widgets/lib/view/screen/search_result_screen.dart
+++ tiny_widgets/lib/view/screen/search_result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -14,19 +15,26 @@ class SearchResultScreen extends GetView<SearchResultController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 100),
-          _image(),
-          Padding(
-            padding: MyConstants.SCREEN_HORIZONTAL_MARGIN,
-            child: Column(
-              children: [
-                _title(),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            _image(),
+            SizedBox(height: 24),
+            Padding(
+              padding: MyConstants.SCREEN_HORIZONTAL_MARGIN,
+              child: Column(
+                children: [
+                  _title(),
+                  SizedBox(height: 12),
+                  _snippetAndLink(),
+                  SizedBox(height: 16),
+                  _dummyText(),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -40,12 +48,51 @@ class SearchResultScreen extends GetView<SearchResultController> {
   }
 
   Widget _title() {
-    return Obx(
-      () => Text(
-        controller.title,
-        style: Theme.of(Get.context!).textTheme.headline6,
+    return SizedBox(
+      width: double.infinity,
+      child: Obx(
+        () => Text(
+          controller.title,
+          style: Theme.of(Get.context!).textTheme.headline4!.copyWith(
+                fontWeight: FontWeight.w800,
+                color: Colors.black,
+                letterSpacing: -0.8,
+              ),
+        ),
       ),
     );
+  }
+
+  Widget _snippetAndLink() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: Get.size.width * 0.4,
+          child: Text(
+            controller.result?.displayLink ?? "",
+            style: Theme.of(Get.context!).textTheme.subtitle2!,
+            maxLines: 1,
+          ),
+        ),
+        SizedBox(
+          width: Get.size.width * 0.5,
+          child: Text(
+            controller.result?.snippet ?? "",
+            style: Theme.of(Get.context!).textTheme.subtitle2!,
+            textAlign: TextAlign.end,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _dummyText() {
+    return Text(MyConstants.DUMMY_TEXT,
+        style: Theme.of(Get.context!).textTheme.bodyText2);
   }
 }
 

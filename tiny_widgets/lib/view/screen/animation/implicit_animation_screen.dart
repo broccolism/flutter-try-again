fc7db595ca9a0dbba3d406e761f5f9631a887e66
broccolism <mile3880@gmail.com>
2022-01-05 23:35:39 +0900

    #13 :sparkles: change container's color, size, borderRadius

--- tiny_widgets/lib/view/screen/animation/implicit_animation_screen.dart
+++ tiny_widgets/lib/view/screen/animation/implicit_animation_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,29 +17,58 @@ class _ImplicitAnimationScreenState extends State<ImplicitAnimationScreen> {
   final String IMAGE_NAME = "assets/images/broccoli.jpg";
   final String SHOW_DETAIL_BUTTON_TEXT = "Show details";
   final String HIDE_DETAIL_BUTTON_TEXT = "Hide details";
+  final String CHANGE_BUTTON_TEXT = "Chnage";
   final String BODY_TEXT = "Type: Broccoli\nAge: 3\nJob: Being in a spoon";
+  final ButtonStyle _buttonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.green[100]!),
+  );
 
-  // TODO: make a controller and let it manage opacity.
+  // TODO: make a controller and let it manage variables.
   double opacity = 0.0;
+  late Color color;
+  late double borderRadius;
+  late double margin;
+
+  @override
+  void initState() {
+    super.initState();
+    color = Colors.deepPurple;
+    borderRadius = _randomBorderRadius();
+    margin = _randomMargin();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
+          physics: ClampingScrollPhysics(),
           child: Column(
             children: [
-              SizedBox(height: 32),
-              _title(),
-              SizedBox(height: 16),
-              _image(),
-              SizedBox(height: 24),
-              _detailButton(),
-              SizedBox(height: 16),
-              _body(),
+              SizedBox(height: 48),
+              _imageAndDetails(),
+              _divider(),
+              SizedBox(height: 48),
+              _animatingShape(),
+              SizedBox(height: 48),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _imageAndDetails() {
+    return Column(
+      children: [
+        _title(),
+        SizedBox(height: 16),
+        _image(),
+        SizedBox(height: 24),
+        _detailButton(),
+        SizedBox(height: 16),
+        _body(),
+      ],
     );
   }
 
@@ -55,9 +86,7 @@ class _ImplicitAnimationScreenState extends State<ImplicitAnimationScreen> {
   Widget _detailButton() {
     return TextButton(
       onPressed: () => setState(() => opacity = 1 - opacity),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(Colors.green[100]!),
-      ),
+      style: _buttonStyle,
       child: Text(
         opacity == 1.0 ? HIDE_DETAIL_BUTTON_TEXT : SHOW_DETAIL_BUTTON_TEXT,
         style: Theme.of(context).textTheme.button,
@@ -67,6 +96,7 @@ class _ImplicitAnimationScreenState extends State<ImplicitAnimationScreen> {
 
   Widget _body() {
     return AnimatedOpacity(
+      curve: Curves.bounceIn,
       opacity: opacity,
       duration: const Duration(seconds: 1),
       child: Text(
@@ -78,5 +108,68 @@ class _ImplicitAnimationScreenState extends State<ImplicitAnimationScreen> {
         textAlign: TextAlign.center,
       ),
     );
+  }
+
+  Widget _divider() {
+    return Column(
+      children: [
+        SizedBox(height: 12),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: 1,
+          color: Colors.grey[400],
+        ),
+        SizedBox(height: 12)
+      ],
+    );
+  }
+
+  Widget _animatingShape() {
+    return Column(
+      children: [
+        SizedBox(
+          width: 128,
+          height: 128,
+          child: AnimatedContainer(
+            curve: Curves.slowMiddle,
+            margin: EdgeInsets.all(margin),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+            duration: const Duration(milliseconds: 500),
+          ),
+        ),
+        SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: _change,
+          style: _buttonStyle,
+          child: Text(
+            CHANGE_BUTTON_TEXT,
+            style: Theme.of(context).textTheme.button,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _change() {
+    setState(() {
+      color = _randomColor();
+      borderRadius = _randomBorderRadius();
+      margin = _randomMargin();
+    });
+  }
+
+  double _randomBorderRadius() {
+    return Random().nextDouble() * 64;
+  }
+
+  double _randomMargin() {
+    return Random().nextDouble() * 64;
+  }
+
+  Color _randomColor() {
+    return Color(0xFFFFFFFF & Random().nextInt(0xFFFFFFFF));
   }
 }

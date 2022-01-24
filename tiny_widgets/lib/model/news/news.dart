3cc05dbffd977b06591dafe8718c478a653e38d4
broccolism <mile3880@gmail.com>
2022-01-24 22:53:55 +0900

    #19 :sparkles: news & article dummy data

---
+++ tiny_widgets/lib/model/news/news.dart
@@ -0,0 +1,27 @@
+class Article {
+  final String title;
+  final String company;
+  final DateTime createdAt;
+  final int replyCount;
+  final String imageUrl;
+  final NewsTheme theme;
+
+  const Article({
+    required this.title,
+    required this.company,
+    required this.createdAt,
+    required this.replyCount,
+    required this.imageUrl,
+    required this.theme,
+  });
+}
+
+class NewsTheme {
+  final int id;
+  final String title;
+
+  const NewsTheme({
+    required this.id,
+    required this.title,
+  });
+}
